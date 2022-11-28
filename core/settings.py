af78a385c9e12a78cbdab87182e6963082ebc0c7
unknown <m-g-j@m-g-jose.lubagahospital.org>
2022-11-28 15:02:16 +0300

    first commit

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9x)t=)n&u@scwle+r)70@1cgzir@n!^ra44*)1^w!$+_bz5k55
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
